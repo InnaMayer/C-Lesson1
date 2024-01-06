5602bd61855237ff20d32b890564f07d63186eec
Inna Kochurkina <inna.mayer.87@bk.ru>
2024-01-06 12:06:00 +0300

    Seminar1/Task1

---
+++ Seminar1/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два целых числа 
+// и проверяет, является ли первое число квадратом второго.
+
+int first_number = 25;
+int second_number = 5;
+
+if(second_number*second_number == first_number){
+    Console.Write("First num is a quater of second num");
+}
+else{
+    Console.Write("First num is not a quater of second num");
+}
