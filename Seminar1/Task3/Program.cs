eb1c24ce4ce1c56c212803066ab7e58647fe0edc
Inna Kochurkina <inna.mayer.87@bk.ru>
2024-01-06 16:43:21 +0300

    Seminar1/Task3

---
+++ Seminar1/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу, которая принимает на вход трехзначное целое
+// число и на выходе показывает сумму первой и последней цифры этого числа.
+
+int num = 424;
+if (num>=100 && num<=999){
+  int num1 = num / 100;
+  int num2 = num % 10;
+  int sum = num1 + num2;
+  Console.Write($"Sum of first and Last digit in numb {num} is {sum}");
+}
+else{
+    Console.Write($"Your numb is not 3 digit");
+}
+
